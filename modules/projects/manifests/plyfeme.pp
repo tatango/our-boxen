79e427e6cb728157e29bf15b74718c151291088a
Dave Liggat <liggsie@gmail.com>
2013-07-24 18:34:57 -0400

    A few project notes

--- modules/projects/manifests/plyfeme.pp
+++ modules/projects/manifests/plyfeme.pp
@@ -18,12 +18,16 @@ class projects::plyfeme {
     ]:
   }
 
-  # TODO:
+  # TODO initial setup:
+  # Database raking not working natively. plyfedbdude cannot connect through socket. Fix.
+
+  # TODO automate:
   # - Hosts file jigging
   # - Set up plyfedbdude user
   # - Set up socket for mysql
   # - Start mysql on reboot
   # - Start redis
+  # - Install hall binary (no package yet on github.com/boxen)
   # - Install certificate
   # - (maybe) install plyfeec2 key
   # - Set up sensible sublime defaults - should be a user level module.
