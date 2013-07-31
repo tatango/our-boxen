a43abf4e885e2bd7bbe982860d1a9f9e29d874a7
Dave Liggat <dliggat@gmail.com>
2013-07-31 10:30:13 -0700

    Onboarding Chris

---
+++ modules/people/manifests/chrislopresto.pp
@@ -0,0 +1,30 @@
+class people::chrislopresto {
+
+  notify { 'class people::chrislopresto declared': }
+
+  # Git global settings.
+  git::config::global {
+    'user.name':        value => 'Chris LoPresto';
+    'user.email':       value => 'chris@plyfe.me';
+    'core.editor':      value => 'vim';
+    'color.ui':         value => 'true';
+
+    # Aliases
+    'alias.s':         value => 'status';
+  }
+
+  # Apps on a per-user basis. Still need to add these entries to Puppetfile.
+  include vlc
+
+  # Homebrew packages on a per-user basis.
+  package {
+    [
+      'tree',
+      'sqlite'
+    ]:
+  }
+
+  # Define the basic dotfiles - engineers will probably want to skip this step and point to their
+  #  own personal dotfile repos.
+  include plyfe::optional::basic_dotfiles
+}
