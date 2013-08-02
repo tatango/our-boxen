4306cdb36c5a003c709afb6b6f1ef39dfc3b2e34
Dave Liggat <dliggat@gmail.com>
2013-08-02 10:38:42 -0700

    Adding more packages from Chris recommendations

--- modules/plyfe/manifests/optional/sublime_text_2.pp
+++ modules/plyfe/manifests/optional/sublime_text_2.pp
@@ -17,18 +17,36 @@ class plyfe::optional::sublime_text_2 {
     require => Repository["dotfiles"],
   }
 
-  # Basic plugins.
+  # Default, useful plugins for everyone.
   sublime_text_2::package { 'GitGutter':
     source => 'jisaacks/GitGutter'
+  }
+  sublime_text_2::package { 'Haml':
+    source => 'handcrafted/handcrafted-haml-textmate-bundle'
+  }
+  sublime_text_2::package { 'Jade':
+    source => 'miksago/jade-tmbundle'
+  }
+  sublime_text_2::package { 'SCSS':
+    source => 'kuroir/SCSS.tmbundle'
+  }
+  sublime_text_2::package { 'Sass':
+    source => 'nathos/sass-textmate-bundle'
+  }
+  sublime_text_2::package { 'SidebarEnhancements':
+    source => 'titoBouzout/SideBarEnhancements'
+  }
+  sublime_text_2::package { 'SublimeAllAutocomplete':
+    source => 'alienhard/SublimeAllAutocomplete'
+  }
+  sublime_text_2::package { 'SublimeLinter':
+    source => 'SublimeLinter/SublimeLinter'
   }
   sublime_text_2::package { 'jQuery':
     source => 'SublimeText/jQuery'
   }
   sublime_text_2::package { 'sublime-text-2-git':
     source => 'kemayo/sublime-text-2-git'
-  }
-  sublime_text_2::package { 'SublimeAllAutocomplete':
-    source => 'alienhard/SublimeAllAutocomplete'
   }
 
 }
