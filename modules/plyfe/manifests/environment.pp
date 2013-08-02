ef72865446cfb73056b289c5547e7594bd5b36b7
Dave Liggat <dliggat@gmail.com>
2013-08-02 11:57:11 -0700

    top level source directory creation

--- modules/plyfe/manifests/environment.pp
+++ modules/plyfe/manifests/environment.pp
@@ -1,6 +1,11 @@
 class plyfe::environment {
   notify { 'class plyfe::environment declared': }
 
+  # Create a top level source directory for plyfe app repos.
+  file { "${boxen::config::srcdir}/plyfe":
+    ensure => "directory",
+  }
+
   include plyfe::apps::gems
   include plyfe::apps::mac
   include plyfe::apps::npm
