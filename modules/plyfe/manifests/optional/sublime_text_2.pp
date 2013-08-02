b492f2e46a373218430767ebb499abead9ba37c8
Dave Liggat <dliggat@gmail.com>
2013-08-02 12:17:44 -0700

    Idempotent directory creation

--- modules/plyfe/manifests/optional/sublime_text_2.pp
+++ modules/plyfe/manifests/optional/sublime_text_2.pp
@@ -5,6 +5,11 @@ class plyfe::optional::sublime_text_2 {
   include sublime_text_2
 
   $base = "/Users/${::luser}/Library/Application Support"
+
+  exec { 'Idempotent creation of User preferences directory':
+    command => "mkdir -p ${base}/Sublime Text 2/Packages/User"
+  }
+
   file { "${base}/Sublime Text 2/Packages/User/Preferences.sublime-settings":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/sublime_text_2/Packages/User/Preferences.sublime-settings",
