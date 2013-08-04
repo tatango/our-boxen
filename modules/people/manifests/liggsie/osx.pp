6efec0196432c950ec4699003e6b4b873feee781
Dave Liggat <dliggat@gmail.com>
2013-08-03 18:49:05 -0700

    Commenting out osx for now

--- modules/people/manifests/liggsie/osx.pp
+++ modules/people/manifests/liggsie/osx.pp
@@ -4,15 +4,15 @@ class people::liggsie::osx {
 
   osx::recovery_message { 'If this Mac is found, please contact: dliggat@gmail.com.': }
 
-  include osx::dock::2d
-  include osx::dock::dim_hidden_apps
+  # include osx::dock::2d
+  # include osx::dock::dim_hidden_apps
 
-  include osx::finder::show_all_on_desktop
-  include osx::finder::unhide_library
+  # include osx::finder::show_all_on_desktop
+  # include osx::finder::unhide_library
 
-  class { 'osx::global::key_repeat_delay': delay => 10 }
-  class { 'osx::global::key_repeat_rate': rate => 2 }
-  include osx::global::disable_autocorrect
-  include osx::global::expand_print_dialog
-  include osx::global::expand_save_dialog
+  # class { 'osx::global::key_repeat_delay': delay => 10 }
+  # class { 'osx::global::key_repeat_rate': rate => 2 }
+  # include osx::global::disable_autocorrect
+  # include osx::global::expand_print_dialog
+  # include osx::global::expand_save_dialog
 }
