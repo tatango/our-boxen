1728f97eccf1889665b71025d69ad39a99734ac0
Dave Liggat <dliggat@gmail.com>
2013-08-03 18:07:03 -0700

    Rename variables for dotfiles

--- modules/people/manifests/liggsie/dotfiles.pp
+++ modules/people/manifests/liggsie/dotfiles.pp
@@ -8,57 +8,57 @@ class people::liggsie::config::dotfiles {
       path     => "${::boxen_srcdir}/dotfiles";
   }
 
-  $my_homedir = "/Users/${::luser}"
+  $homedir = "/Users/${::luser}"
 
-  file { "${my_homedir}/.gvimrc":
+  file { "${homedir}/.gvimrc":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/gvimrc",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/.vimrc":
+  file { "${homedir}/.vimrc":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/vimrc",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/.bash_profile":
+  file { "${homedir}/.bash_profile":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/bash_profile",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/.bashrc":
+  file { "${homedir}/.bashrc":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/bashrc",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/.gitignore":
+  file { "${homedir}/.gitignore":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/gitignore",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/.synergy.conf":
+  file { "${homedir}/.synergy.conf":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/synergy.conf",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/Library/Preferences/com.manytricks.Moom.plist":
+  file { "${homedir}/Library/Preferences/com.manytricks.Moom.plist":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/plists/com.manytricks.Moom.plist",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/Library/Preferences/com.googlecode.iterm2.plist":
+  file { "${homedir}/Library/Preferences/com.googlecode.iterm2.plist":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/plists/com.googlecode.iterm2.plist",
     require => Repository["dotfiles"],
   }
 
-  file { "${my_homedir}/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist":
+  file { "${homedir}/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist":
     ensure  => link,
     target  => "${::boxen_srcdir}/dotfiles/plists/com.runningwithcrayons.Alfred-Preferences.plist",
     require => Repository["dotfiles"],
