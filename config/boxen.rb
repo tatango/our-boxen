dba04a334c1c99665eb671a470b5f002372f3a75
Dave Liggat <dliggat@gmail.com>
2013-08-06 17:31:22 -0700

    Changing user -> whoami

--- config/boxen.rb
+++ config/boxen.rb
@@ -25,4 +25,4 @@
 # ENV['BOXEN_REPO_URL_TEMPLATE'] = 'https://github.yourdomain.com/%s'
 
 # TODO: Try this out.
-ENV["BOXEN_SRC_DIR"] = "/Users/#{user}/git-boxen"
+ENV["BOXEN_SRC_DIR"] = "/Users/#{`whoami`.strip}/git-boxen"
