21d551ae4e5f87cdab2aec9b378851ec6d646399
Dave Liggat <dliggat@gmail.com>
2013-08-07 08:25:31 -0700

    os x stuff for a run through test

--- modules/people/manifests/liggsie/osx.pp
+++ modules/people/manifests/liggsie/osx.pp
@@ -2,39 +2,35 @@ class people::liggsie::osx {
 
   notify { 'class people::liggsie::osx declared': }
 
-  include osx
-  # TODO: make this work.
-  osx::recovery_message { 'If this Mac is found, please contact: dliggat@gmail.com.': }
-
   include osx::dock::2d
   include osx::dock::dim_hidden_apps
 
-  # include osx::finder::show_all_on_desktop
-  # include osx::finder::unhide_library
+  include osx::finder::show_all_on_desktop
+  include osx::finder::unhide_library
 
-  # class { 'osx::global::key_repeat_delay': delay => 10 }
-  # class { 'osx::global::key_repeat_rate': rate => 2 }
-  # include osx::global::disable_autocorrect
-  # include osx::global::expand_print_dialog
-  # include osx::global::expand_save_dialog
+  class { 'osx::global::key_repeat_delay': delay => 10 }
+  class { 'osx::global::key_repeat_rate': rate => 2 }
 
-  #TODO:
-  #   boxen::osx_defaults {
-  #   'Disable the dashboard':
-  #     ensure => present,
-  #     key    => 'mcx-disabled',
-  #     domain => 'com.apple.dashboard',
-  #     value  => 'true',
-  #     type   => 'bool',
-  #     user   => $::boxen_user;
+  include osx::global::disable_autocorrect
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
 
-  #   'Don’t show Dashboard as a Space':
-  #     ensure => present,
-  #     key    => 'dashboard-in-overlay',
-  #     domain => 'com.apple.dock',
-  #     value  => 'true',
-  #     type   => 'bool',
-  #     user   => $::boxen_user;
-  # }
+  boxen::osx_defaults {
+    'Disable the dashboard':
+      ensure => present,
+      key    => 'mcx-disabled',
+      domain => 'com.apple.dashboard',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+
+    'Don’t show Dashboard as a Space':
+      ensure => present,
+      key    => 'dashboard-in-overlay',
+      domain => 'com.apple.dock',
+      value  => 'true',
+      type   => 'bool',
+      user   => $::boxen_user;
+  }
 
 }
