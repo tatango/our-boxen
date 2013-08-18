d499c09a6a7dedf0086ed3cd2150de15a44fc9d8
Dave Liggat <dliggat@gmail.com>
2013-08-18 12:35:19 -0400

    Adding sublime text symlink

--- modules/people/manifests/dliggat/symlinks.pp
+++ modules/people/manifests/dliggat/symlinks.pp
@@ -7,6 +7,11 @@ class people::dliggat::symlinks {
     target  => "/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc"
   }
 
+  file { "${boxen::config::homedir}/bin/subl":
+    ensure  => link,
+    target  => "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+  }
+
   file { "/Users/${::luser}/Documents/DocumentSync":
     ensure  => link,
     target  => "/Users/${::luser}/Dropbox/DocumentSync"
