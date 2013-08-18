8ee4be2e47968409473edb269453f28a226ba373
Dave Liggat <dliggat@gmail.com>
2013-08-18 12:49:47 -0400

    Updating git aliases

--- modules/people/manifests/dliggat/git.pp
+++ modules/people/manifests/dliggat/git.pp
@@ -3,16 +3,16 @@ class people::dliggat::git {
   notify { 'class people::dliggat::git declared': }
 
   git::config::global {
-    'user.name':        value => 'Dave Liggat';
-    'user.email':       value => 'dliggat@gmail.com';
-    'core.editor':      value => 'vim';
-    'color.ui':         value => 'true';
+    'user.name':     value => 'Dave Liggat';
+    'user.email':    value => 'dliggat@gmail.com';
+    'core.editor':   value => 'vim';
+    'color.ui':      value => 'true';
 
-    'alias.b':         value => 'branch';
-    'alias.ch':        value => 'checkout';
-    'alias.cm':        value => 'checkout master';
-    'alias.co':        value => 'commit';
-    'alias.s':         value => 'status';
+    'alias.b':       value => 'branch';
+    'alias.ch':      value => 'checkout';
+    'alias.cm':      value => 'checkout master';
+    'alias.co':      value => 'commit';
+    'alias.s':       value => 'status -sb';
   }
 
 }
