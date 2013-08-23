6f34e9a4f78cf85ff69de63709697437ff5b75dd
Chris LoPresto <chrislopresto@gmail.com>
2013-08-23 09:05:29 -0400

    Rename plyfe_embed project to plyfe_preview

---
+++ modules/projects/manifests/plyfe_preview.pp
@@ -0,0 +1,15 @@
+class projects::plyfe_preview {
+
+  notify { 'class projects::plyfe_preview declared': }
+
+  boxen::project { 'plyfe_preview':
+    ruby          => '2.0.0',
+    dir           => "${boxen::config::srcdir}/plyfe-preview",
+    source        => 'plyfe/plyfe-preview',
+  }
+
+  host { "preview-development.plyfe.me":
+    ip => "127.0.0.1",
+  }
+
+}
