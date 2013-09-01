a0441aa8e6bf63e1533f62fb42c2194a87cc1638
Dave Liggat <dliggat@gmail.com>
2013-08-31 20:14:39 -0400

    Added new login items

--- modules/people/manifests/dliggat/loginitems.pp
+++ modules/people/manifests/dliggat/loginitems.pp
@@ -18,19 +18,34 @@ class people::dliggat::loginitems {
       path    => '/Applications/Google Chrome.app',
       hidden  => true;
 
-   'Fantastical':
+    'Safari':
+      name    => 'Safari',
+      path    => '/Applications/Safari.app',
+      hidden  => true;
+
+    'Fantastical':
       name    => 'Fantastical',
       path    => '/Applications/Fantastical.app',
       hidden  => true;
 
-   'Mail':
+    'Mail':
       name    => 'Mail',
       path    => '/Applications/Mail.app',
+      hidden  => true;
+
+    'Evernote':
+      name    => 'Evernote',
+      path    => '/Applications/Evernote.app',
       hidden  => true;
 
     'Moom':
       name    => 'Moom',
       path    => '/Applications/Moom.app',
+      hidden  => false;
+
+    'DeskConnect':
+      name    => 'DeskConnect',
+      path    => '/Applications/DeskConnect.app',
       hidden  => false;
   }
 
