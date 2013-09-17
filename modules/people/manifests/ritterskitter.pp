140dcf92056287cd185851fcae29755d5556ea39
Dave Liggat <dliggat@gmail.com>
2013-09-17 11:53:39 -0400

    Adding a personal manifest for Angie

---
+++ modules/people/manifests/ritterskitter.pp
@@ -0,0 +1,29 @@
+class people::ritterskitter {
+
+  notify { 'class people::ritterskitter declared': }
+
+  # Git global settings.
+  git::config::global {
+    'user.name':        value => 'Angela Fagg';
+    'user.email':       value => 'a.mariefagg@gmail.com';
+    'core.editor':      value => 'vim';
+    'color.ui':         value => 'true';
+
+    # Aliases
+    'alias.s':         value => 'status';
+  }
+
+  # Apps on a per-user basis. Still need to add these entries to Puppetfile.
+  include vlc
+
+  # Homebrew packages on a per-user basis.
+  package {
+    [
+      'tree'
+    ]:
+  }
+
+  # Define the basic dotfiles - engineers will probably want to skip this step and point to their
+  #  own personal dotfile repos.
+  include plyfe::optional::basic_dotfiles
+}
