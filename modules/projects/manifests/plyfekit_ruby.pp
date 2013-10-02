18d21c64904a09d5b5a7e6b3641437bec671e408
Dave Liggat <dliggat@gmail.com>
2013-10-02 10:55:41 -0400

    Adding denny manifest

--- modules/projects/manifests/plyfekit_ruby.pp
+++ modules/projects/manifests/plyfekit_ruby.pp
@@ -3,8 +3,6 @@ class projects::plyfekit_ruby {
   notify { 'class projects::plyfekit_ruby declared': }
 
   boxen::project { 'plyfekit_ruby':
-    mysql         => true,
-    redis         => true,
     ruby          => '1.9.3',
     dir           => "${boxen::config::srcdir}/plyfekit-ruby",
     source        => 'plyfe/plyfekit-ruby',
