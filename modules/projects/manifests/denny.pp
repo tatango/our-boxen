446676e69335ed7a4e2a0a66041897015d69c940
Dave Liggat <dliggat@gmail.com>
2013-10-17 11:13:55 -0400

    Appropriate mysql/redis settings for denny and mysql

--- modules/projects/manifests/denny.pp
+++ modules/projects/manifests/denny.pp
@@ -3,6 +3,8 @@ class projects::denny {
   notify { 'class projects::denny declared': }
 
   boxen::project { 'denny':
+    mysql         => true,
+    redis         => false,
     ruby          => '2.0.0',
     dir           => "${boxen::config::srcdir}/denny",
     source        => 'plyfe/denny',
