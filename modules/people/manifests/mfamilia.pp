d8fd2bb4a4f94bbd25a26ad4cf17393e3643c6f1
Dave Liggat <dliggat@gmail.com>
2013-10-21 16:35:56 -0400

    Added mfamilias personal manifest

---
+++ modules/people/manifests/mfamilia.pp
@@ -0,0 +1,30 @@
+class people::mfamilia {
+
+  notify { 'class people::mfamilia declared': }
+
+  # Git global settings.
+  git::config::global {
+    'user.name':        value => 'Manolo';
+    'user.email':       value => 'manolo@plyfe.me';
+    'core.editor':      value => 'vim';
+    'color.ui':         value => 'true';
+
+    # Aliases
+    'alias.s':         value => 'status';
+  }
+
+  # Apps on a per-user basis. Still need to add these entries to Puppetfile.
+  include vlc
+  include sourcetree
+
+  # Homebrew packages on a per-user basis.
+  package {
+    [
+      'tree'
+    ]:
+  }
+
+  # Define the basic dotfiles - engineers will probably want to skip this step and point to their
+  #  own personal dotfile repos.
+  include plyfe::optional::basic_dotfiles
+}
