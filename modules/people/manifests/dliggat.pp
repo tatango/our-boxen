2e18fc5b2fd58234b5e1cd2b4d347a8e1fc4bfa5
Dave Liggat <dliggat@gmail.com>
2013-11-28 12:26:23 -0500

    Commenting out osx module stuff

--- modules/people/manifests/dliggat.pp
+++ modules/people/manifests/dliggat.pp
@@ -7,7 +7,7 @@ class people::dliggat {
   include people::dliggat::git
   include people::dliggat::homebrew
   include people::dliggat::symlinks
-  include people::dliggat::loginitems
+  # include people::dliggat::loginitems
   # include people::dliggat::osx
   include people::dliggat::sublime_text_2
 
