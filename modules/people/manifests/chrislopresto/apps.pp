a6cfff3aec55c6236d11c579b698605ecb79629f
Chris LoPresto <chrislopresto@gmail.com>
2013-12-03 10:46:53 -0500

    Update chrislopresto manifest

--- modules/people/manifests/chrislopresto/apps.pp
+++ modules/people/manifests/chrislopresto/apps.pp
@@ -2,17 +2,7 @@ class people::chrislopresto::apps {
 
   notify { 'class people::chrislopresto::apps declared': }
 
-  include chrome
-  include dropbox
-  include firefox
-  include heroku
   include iterm2::stable
-  include rdio
   include sequel_pro
-  include sublime_text_2
-  include tower
-  include transmission
-  include vim
-  include wget
 
 }
