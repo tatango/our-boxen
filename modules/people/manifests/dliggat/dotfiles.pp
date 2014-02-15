1c0ea7c8af86c5b9fa1ffa156446ed99e2fda349
Dave Liggat <dliggat@gmail.com>
2014-02-15 14:34:06 -0500

    Removing unnecessary symlinks

--- modules/people/manifests/dliggat/dotfiles.pp
+++ modules/people/manifests/dliggat/dotfiles.pp
@@ -40,28 +40,4 @@ class people::dliggat::dotfiles {
     require => Repository["dotfiles"],
   }
 
-  file { "${homedir}/.synergy.conf":
-    ensure  => link,
-    target  => "${::boxen_srcdir}/dotfiles/synergy.conf",
-    require => Repository["dotfiles"],
-  }
-
-  file { "${homedir}/Library/Preferences/com.manytricks.Moom.plist":
-    ensure  => link,
-    target  => "${::boxen_srcdir}/dotfiles/plists/com.manytricks.Moom.plist",
-    require => Repository["dotfiles"],
-  }
-
-  file { "${homedir}/Library/Preferences/com.googlecode.iterm2.plist":
-    ensure  => link,
-    target  => "${::boxen_srcdir}/dotfiles/plists/com.googlecode.iterm2.plist",
-    require => Repository["dotfiles"],
-  }
-
-  file { "${homedir}/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist":
-    ensure  => link,
-    target  => "${::boxen_srcdir}/dotfiles/plists/com.runningwithcrayons.Alfred-Preferences.plist",
-    require => Repository["dotfiles"],
-  }
-
 }
